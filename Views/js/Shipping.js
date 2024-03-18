03cd8e744f2321b7bccb693030ace04002576060
Steponas <steponaskam@gmail.com>
2024-03-18 19:58:59 +0200

    Added new file for JS (work in progress)

---
+++ Views/js/Shipping.js
@@ -0,0 +1,111 @@
+
+$(document).ready(function () { 
+    document.getElementById('saveButton').addEventListener('click', async function() {
+        var token = document.getElementById('token').value;
+        var firstName = document.getElementById('firstName').value;
+        var lastName = document.getElementById('lastName').value;
+        var company = document.getElementById('company').value;
+        var country = document.getElementById('country').value;
+        var address1 = document.getElementById('address1').value;
+        var address2 = document.getElementById('address2').value;
+        var postcode = document.getElementById('postcode').value;
+        var city = document.getElementById('city').value;
+        var phone = document.getElementById('phone').value;
+        var phoneMobile = document.getElementById('phoneMobile').value;
+    
+        const postButton = async () => {
+            const response = await fetch('http://localhost:8000/api/shipment/save/', {
+                method: "POST",
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    company: company,
+                    country: country,
+                    address1: address1,
+                    address2: address2,
+                    postcode: postcode,
+                    city: city,
+                    phone: phone,
+                    phoneMobile: phoneMobile
+                }),
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                }
+            });
+    
+            return response.json();
+        };
+    
+        const response = await postButton();
+    });
+});
+
+
+        
+    //     axios.post('http://localhost:8000/api/shipment/save/', {
+    //         firstName: firstName,
+    //         lastName: lastName,
+    //         company: company,
+    //         country: country,
+    //         address1: address1,
+    //         address2: address2,
+    //         postcode: postcode,
+    //         city: city,
+    //         phone: phone,
+    //         phoneMobile: phoneMobile
+    //     }, {
+    //         headers: {
+    //             'Authorization': 'Bearer ' + token
+    //         }
+    //     })
+    //     .then(function (response) {
+    //         console.log('Response:', response.data);
+    //         if(response.status === 201) {
+    //             var shipmentId = response.data;
+
+    //             document.getElementById('saveButton').style.display = 'none';
+
+    //             var printButton = document.createElement('button');
+    //             printButton.innerText = 'Print Label';
+    //             printButton.className = 'btn btn-primary';
+    //             printButton.setAttribute('id', 'printButton');
+    //             printButton.setAttribute('data-shipment-id', shipmentId);
+    //             printButton.addEventListener('click', function() {
+    //                 event.preventDefault();
+    //                 axios.get('http://localhost:8000/api/shipment/print/' + shipmentId, {
+    //                     headers: {
+    //                         'Authorization': 'Bearer ' + token
+    //                     }
+    //                 })
+    //                 .then(function(response) {
+    //                     console.log('Label printed:', response.data);
+    //                     var pdfContent = response.data;
+    //                     console.log('var pdfContent= ', pdfContent);
+    //                     var decodedData = atob(pdfContent);
+    //                     console.log('var decodedData= ', decodedData);
+                        
+    //                     const byteCharacters = decodedData;
+    //                     const byteNumbers = new Array(byteCharacters.length);
+    //                     for (let i = 0; i < byteCharacters.length; i++) {
+    //                         byteNumbers[i] = byteCharacters.charCodeAt(i);
+    //                     }
+    //                     const byteArray = new Uint8Array(byteNumbers);
+    //                     const blob = new Blob([byteArray], { type: 'application/pdf' });
+
+    //                     const url = URL.createObjectURL(blob);
+
+    //                     window.open(url);
+
+    //                 })
+    //                 .catch(function(error) {
+    //                     console.error('Error printing label:', error);
+    //                 });
+    //             });
+    //             document.getElementById('saveButton').parentNode.appendChild(printButton);
+    //         }
+    //     })
+    //     .catch(function (error) {
+    //         console.error('Error:', error);
+    //     });
+    // });
